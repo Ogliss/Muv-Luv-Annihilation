a222cb01a2e30fc42cf9033fb709813ada9f91a7
Taranchuk <nfhfyxer@gmail.com>
2021-09-26 16:26:54 +0300

    update

---
+++ Source/DamageWorker_Tear.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace MuvLuvAnnihilation
+{
+
+	public class DamageWorker_Tear : DamageWorker_AddInjury
+	{
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+			if (thing is Pawn pawn && dinfo.Amount >= 15)
+            {
+				if (Rand.Chance(0.8f))
+                {
+					List<BodyPartRecord> list = (from x in pawn.RaceProps.body.AllParts
+												 where !pawn.health.hediffSet.PartIsMissing(x)
+												&& x.depth == BodyPartDepth.Outside
+												&& x.coverage > 0.1f
+												 select x).ToList<BodyPartRecord>();
+
+					if (list.TryRandomElement<BodyPartRecord>(out var bodyPartRecord))
+					{
+						var missingBodyPart = HediffMaker.MakeHediff(HediffDefOf.MissingBodyPart, pawn, bodyPartRecord);
+						pawn.health.AddHediff(missingBodyPart);
+					}
+				}
+			}
+
+			return base.Apply(dinfo, thing);
+        }
+    }
+}
--- Source/Harmony/HarmonyPatches.cs
+++ Source/Harmony/HarmonyPatches.cs
@@ -15,7 +15,6 @@ namespace MuvLuvAnnihilation
         {
             if (___pawn is BetaEater)
             {
-                ___pawn.jobs.debugLog = true;
                 if (___pawn.jobs.curDriver is JobDriver_AttackMelee || ___pawn.jobs.curDriver is JobDriver_AttackStatic)
                 {
                     __state = ___pawn.jobs.curJob.targetA.Pawn;
@@ -33,7 +32,7 @@ namespace MuvLuvAnnihilation
         }
         private static void Postfix(Pawn_JobTracker __instance, Pawn ___pawn, Pawn __state, JobCondition condition, ref bool startNewJob, bool canReturnToPool = true)
         {
-            if (___pawn.needs.food.CurLevel < 1f && __state != null && __state.RaceProps.IsFlesh && (__state.Corpse != null || __state.Downed))
+            if (___pawn.needs?.food != null && ___pawn.needs.food.CurLevel < 1f && __state != null && __state.RaceProps.IsFlesh && (__state.Corpse != null || __state.Downed))
             {
                 Thing target = __state.Corpse != null ? __state.Corpse as Thing : __state;
                 if (___pawn.CanReserveAndReach(target, PathEndMode.ClosestTouch, Danger.Deadly))
